99a879aa86e521c4439623a6147ef0927a8f9165
Drew <drc920630@gmail.com>
2023-02-08 15:58:19 +0900

    점포 찾기까지 완료

--- javascript/starbucksJavaS.js
+++ javascript/starbucksJavaS.js
@@ -1,3 +1,4 @@
+// 검색창
 function Toggleclick () {
   const Clickline = document.querySelector('.material-symbols-outlined');
   const Inputline = document.querySelector('.search-extend');
@@ -13,3 +14,14 @@ function Toggleclick () {
  };
 };
 
+
+// 스크롤이벤트1 엘살바도르
+const Elsal = document.querySelector('.elsalvador__item')
+
+document.addEventListener("scroll" () => {
+  if (window.pageYOffset > 5) {
+    Elsal.classList.add("on");
+   } else {
+    Elsal.classList.remove("on");
+   }
+});
